2d8ca4515d78f07644c320d423b0d600839046e7
dulal71 <ahmeddulal4211@gmail.com>
2026-02-24 00:54:45 +0600

    add functionaity in rejected section

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const totalCount = document.getElementById("total");
 totalCount.innerText = childNode;
 
 //total Interview job
-
-
 const totalInterview = document.getElementById("total-interview");
+//total rejected job
+const totalreject = document.getElementById("rejected");
 
 // available-job
 
@@ -70,30 +70,119 @@ allJob.classList.add("hidden");
 
 // create Object
 let interviewList=[];
+let rejectList=[];
 allJob.addEventListener("click" ,function(event){
-  const parentNode = event.target.parentNode.parentNode.parentNode;
- 
-  const jobName = parentNode.querySelector(".job-name").innerText;
+  if(event.target.classList.contains("btn-interview-selected")){
+const parentNode = event.target.parentNode.parentNode.parentNode;
+ const jobName = parentNode.querySelector(".job-name").innerText;
   const duty =parentNode.querySelector(".duty").innerText;
   const salary =parentNode.querySelector(".salary").innerText;
   const condition =parentNode.querySelector(".condition").innerText;
-  const postion =parentNode.querySelector(".postion").innerText;
+  const position =parentNode.querySelector(".position").innerText;
   
+  // object
   const jobInfo={
     jobName,
     duty,
     salary,
     condition,
-    postion
+    position
   }
   
-const jobExist = interviewList.find(job=> job.jobName=== jobInfo.jobName);
+const jobExist =  interviewList.find(job=> job.jobName=== jobInfo.jobName);
 if(!jobExist){
-  interviewList.push(jobInfo);  
+ interviewList.push(jobInfo);  
 }
 totalInterview.innerText=interviewList.length;
-
+reminderInterview();
+  }else if(event.target.classList.contains("btn-rejected-selected")){
+   const parentNode = event.target.parentNode.parentNode.parentNode;
+ const jobName = parentNode.querySelector(".job-name").innerText;
+  const duty =parentNode.querySelector(".duty").innerText;
+  const salary =parentNode.querySelector(".salary").innerText;
+  const condition =parentNode.querySelector(".condition").innerText;
+  const position =parentNode.querySelector(".position").innerText;
+  
+  // object
+  const jobInfo={
+    jobName,
+    duty,
+    salary,
+    condition,
+    position
+  }
+  
+const jobExist =  rejectList.find(job=> job.jobName=== jobInfo.jobName);
+if(!jobExist){
+   rejectList.push(jobInfo);  
+}
+totalreject.innerText= rejectList.length;
+reminderRejected();
+  }
+  
 })
+
+
+
+
+
+
+
+ function reminderInterview(){
+  interviewJob.innerHTML=``;
+  for(const job of interviewList ){
+    // console.log("job",job)
+    const div = document.createElement("div");
+    div.className = 'bg-base-100 p-4 flex justify-between shadow';
+    div.innerHTML=`
+    <div class="job-contant space-y-4 ">
+    <div>
+        <h3 class="job-name font-bold text-xl">TechCorp Industries</h3>
+<p class="duty text-[#64748B]">Senior Frontend Developer</p>
+</div>
+
+<p  class="salary text-[#64748B]">San Francisco, CA • Full-time •$130,000 - $175,000</p>
+<p id="status"class="condition bg-gray-200 inline-block px-2 py-1">Not Applied</p>
+<p class="postion text-gray-600">We are looking for an experienced Frontend Developer to build scalable web applications using React and TypeScript. You will work with a talented team on cutting-edge projects.</p>
+<div class="flex gap-2">
+<button id="interview-btn" class="btn-interview-selected btn btn-dash btn-success">Interview</button>
+<button id="iejected-btn"  class="btn btn-dash btn-error">Rejected</button>
+</div>
+</div>
+<button id="delete-btn" class="w-8 h-8 flex rounded-full border border-gray-200 text-[#64748B] p-2"><i class="fa-regular fa-trash-can"></i></button>
+   `
+   interviewJob.appendChild(div);
+  }
+
+
+ }
+ function reminderRejected(){
+  rejectedJob.innerHTML=``;
+  for(const job of rejectList ){
+    // console.log("job",job)
+    const div = document.createElement("div");
+    div.className = 'bg-base-100 p-4 flex justify-between shadow';
+    div.innerHTML=`
+    <div class="job-contant space-y-4 ">
+    <div>
+        <h3 class="job-name font-bold text-xl">TechCorp Industries</h3>
+<p class="duty text-[#64748B]">Senior Frontend Developer</p>
+</div>
+
+<p  class="salary text-[#64748B]">San Francisco, CA • Full-time •$130,000 - $175,000</p>
+<p id="status"class="condition bg-gray-200 inline-block px-2 py-1">Not Applied</p>
+<p class="postion text-gray-600">We are looking for an experienced Frontend Developer to build scalable web applications using React and TypeScript. You will work with a talented team on cutting-edge projects.</p>
+<div class="flex gap-2">
+<button id="interview-btn" class="btn-interview-selected btn btn-dash btn-success">Interview</button>
+<button id="iejected-btn"  class="btn btn-dash btn-error">Rejected</button>
+</div>
+</div>
+<button id="delete-btn" class="w-8 h-8 flex rounded-full border border-gray-200 text-[#64748B] p-2"><i class="fa-regular fa-trash-can"></i></button>
+   `
+   rejectedJob.appendChild(div);
+  }
+
+ }
 
 
 
