00e83da94d210c03151f4dbc65d591aa459ea413
dulal71 <ahmeddulal4211@gmail.com>
2026-02-26 01:25:06 +0600

    add job card

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+//get all job section
+const allJob = document.getElementById('all-job');
+const interviewJob = document.getElementById("interview-job");
+const rejectedJob=document.getElementById("rejected-job");
+
 
 let currentStatus = "all";
 const activeStyle = ['bg-primary', 'btn-soft', 'text-white'];
-
 const inActiveStyle = ['btn-soft', 'bg-white'];
+
 //  Toggle Button and get all tab button
 function toggleBtn(tab){ 
                                           //id=all-btn //id="interview-btn"//rejected-btn
@@ -21,6 +26,47 @@ function toggleBtn(tab){
     }
 
  }
+ const allSection =[allJob,interviewJob,rejectedJob]
+ for(const section of allSection ){
+   section.classList.add("hidden");
+ }
+ if(tab === "all"){
+   allJob.classList.remove("hidden"); 
+ }else if(tab==='interview'){
+    interviewJob.classList.remove("hidden"); 
+ }else{
+   rejectedJob.classList.remove("hidden");
+ }
 }
 
 toggleBtn(currentStatus);
+
+
+// total count 
+const totalCount = document.getElementById("total");
+totalCount.innerText = allJob.children.length;
+const totalInterview = document.getElementById("total-interview");
+ const totalreject = document.getElementById("total-rejected");
+
+
+
+ const mainContainer=document.getElementById("job-container");
+ mainContainer.addEventListener("click",function(event){
+const clickElement=event.target;
+console.log(clickElement.parentNode.parentNode.parentNode);
+const card = clickElement.closest('.job-card');
+const cardParent = card.parentNode;
+console.log(cardParent);
+const status = card.querySelector(".condition");
+if(clickElement.classList.contains("btn-interview-selected")){
+   interviewJob.appendChild(card);
+   status.innerText= "Interview";
+}
+if(clickElement.classList.contains("btn-rejected-selected")){
+rejectedJob.appendChild(card);
+ status.innerText= "Rejected";
+}
+if(clickElement.classList.contains("delete")){
+cardParent.removeChild(card);
+}
+ })
